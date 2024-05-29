85dcf70660830688a14964303c7cef245bf99af7
MiguelVilla1 <mhvillah@icloud.com>
2024-05-28 19:57:58 -0700

    engine and trim removal

--- model/Ford.py
+++ model/Ford.py
@@ -18,8 +18,6 @@ def create_table():
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             model_name TEXT NOT NULL,
             year INTEGER NOT NULL,
-            engine TEXT NOT NULL,
-            trim TEXT NOT NULL,
             price REAL NOT NULL,
             horsepower INTEGER NOT NULL
         )
@@ -31,19 +29,19 @@ def create_table():
 @app.route('/populate_data', methods=['GET'])
 def populate_data():
     car_data = [
-        ('Mustang', 2023, 'V8', 'GT', 55000.0, 450),
-        ('F-150', 2022, 'V6', 'XLT', 45000.0, 400),
-        ('Escape', 2021, 'I4', 'Titanium', 35000.0, 250),
-        ('Explorer', 2023, 'V6', 'Limited', 50000.0, 365),
-        ('Bronco', 2023, 'I4', 'Base', 35000.0, 300),
-        ('Escape', 2024, 'I4', 'Base', 29345.0, 180)  # New 2024 Ford Escape data
+        ('Mustang', 2023, 55000.0, 450),
+        ('F-150', 2022, 45000.0, 400),
+        ('Escape', 2021, 35000.0, 250),
+        ('Explorer', 2023, 50000.0, 365),
+        ('Bronco', 2023, 35000.0, 300),
+        ('Escape', 2024, 29345.0, 180)
     ]
 
     conn = connect_db()
     cursor = conn.cursor()
     cursor.executemany('''
-        INSERT INTO ford_models (model_name, year, engine, trim, price, horsepower)
-        VALUES (?, ?, ?, ?, ?, ?)
+        INSERT INTO ford_models (model_name, year, price, horsepower)
+        VALUES (?, ?, ?, ?)
     ''', car_data)
     conn.commit()
     conn.close()
@@ -62,10 +60,8 @@ def get_cars():
             'id': row[0],
             'model_name': row[1],
             'year': row[2],
-            'engine': row[3],
-            'trim': row[4],
-            'price': row[5],
-            'horsepower': row[6]
+            'price': row[3],
+            'horsepower': row[4]
         })
     return jsonify(cars)
 
@@ -100,10 +96,8 @@ def sort_cars():
             'id': row[0],
             'model_name': row[1],
             'year': row[2],
-            'engine': row[3],
-            'trim': row[4],
-            'price': row[5],
-            'horsepower': row[6]
+            'price': row[3],
+            'horsepower': row[4]
         })
 
     return jsonify(cars)
